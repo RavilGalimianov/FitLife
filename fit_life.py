b2a881d0a2ca6ed7405bade5ee99b030ee6ce962
Ravil Galimianov <pelotof@gmail.com>
2026-06-27 04:57:34 +0300

    отформатировал код в соответствии с PEP8

--- fit_life.py
+++ fit_life.py
@@ -25,7 +25,8 @@ while True:
 # проверка на ошибку ввода пользователя строки вместо числа
 while True:
     try:
-        user_height = float(input("Введите Ваш рост(в метрах, например, 1.75):"))
+        promt = "Введите Ваш рост(в метрах, например, 1.75):"
+        user_height = float(input(promt))
         break
     except ValueError:
         print("Введите пожалуйста число!")
@@ -37,7 +38,10 @@ bmi = round((user_weight / (user_height**2)), 1)
 water_mliters = user_weight * WATER_PER_KG
 water_liters = water_mliters / ML_IN_LITER
 
-print(f"Отчет для пользователя: {user_name.capitalize()} ({user_age} л.)\
-    \nТвой Индекс Массы Тела:\
-      {bmi}\nРекомендуемая норма воды: {water_liters:.1f} л. в день\
-    \n\nРасчет окончен. Будьте здоровы!")
+
+print(
+    f"Отчет для пользователя: {user_name.capitalize()} ({user_age} л.)\n"
+    f"Твой Индекс Массы Тела:{bmi}\n"
+    f"Рекомендуемая норма воды: {water_liters:.1f} л. в день\n\n"
+    f"Расчет окончен. Будьте здоровы!"
+)
