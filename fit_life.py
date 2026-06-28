667dbe2a04f1366fc0e133f1d2a6e1dfa573aafe
Ravil Galimianov <pelotof@gmail.com>
2026-06-28 16:25:03 +0300

    добавил функцию main и добавил в инпуты замену запятой на точку

--- fit_life.py
+++ fit_life.py
@@ -4,44 +4,49 @@ WATER_PER_KG = 30
 ML_IN_LITER = 1000
 
 
-user_name = input("Здравствуйте, Введите Ваше Имя:")
+def main():
+    user_name = input("Здравствуйте, Введите Ваше Имя:")
+    formatted_name = user_name.capitalize()
+    
+    # проверка на ошибку ввода пользователя строки вместо числа
+    while True:
+        try:
+            user_age = int(input("Введите Ваш возраст:"))
+            break
+        except ValueError:
+            print("Введите пожалуйста число!")
 
-# проверка на ошибку ввода пользователя строки вместо числа
-while True:
-    try:
-        user_age = int(input("Введите Ваш возраст:"))
-        break
-    except ValueError:
-        print("Введите пожалуйста число!")
+    # проверка на ошибку ввода пользователя строки вместо числа
+    while True:
+        try:
+            user_weight = float(input("Введите Ваш вес(в кг):").replace(",", '.'))
+            break
+        except ValueError:
+            print("Введите пожалуйста число!")
 
-# проверка на ошибку ввода пользователя строки вместо числа
-while True:
-    try:
-        user_weight = float(input("Введите Ваш вес(в кг):"))
-        break
-    except ValueError:
-        print("Введите пожалуйста число!")
+    # проверка на ошибку ввода пользователя строки вместо числа
+    while True:
+        try:
+            promt = "Введите Ваш рост(в метрах, например, 1.75):"
+            user_height = float(input(promt).replace(",", '.'))
+            break
+        except ValueError:
+            print("Введите пожалуйста число!")
 
-# проверка на ошибку ввода пользователя строки вместо числа
-while True:
-    try:
-        promt = "Введите Ваш рост(в метрах, например, 1.75):"
-        user_height = float(input(promt))
-        break
-    except ValueError:
-        print("Введите пожалуйста число!")
+    # расчёт индекса массы тела
+    bmi = round((user_weight / (user_height**2)), 1)
 
-# расчёт индекса массы тела
-bmi = round((user_weight / (user_height**2)), 1)
+    # расчёт рекомендуемой нормы воды
+    water_mliters = user_weight * WATER_PER_KG
+    water_liters = water_mliters / ML_IN_LITER
+    return formatted_name, user_age, user_weight, user_height, bmi, water_liters
 
-# расчёт рекомендуемой нормы воды
-water_mliters = user_weight * WATER_PER_KG
-water_liters = water_mliters / ML_IN_LITER
 
-formatted_name = user_name.capitalize()
-print(
-    f"Отчет для пользователя: {formatted_name} ({user_age} л.)\n"
-    f"Твой Индекс Массы Тела:{bmi}\n"
-    f"Рекомендуемая норма воды: {water_liters:.1f} л. в день\n\n"
-    f"Расчет окончен. Будьте здоровы!",
-)
+if __name__ == "__main__":
+    formatted_name, user_age, user_weight, user_height, bmi, water_liters = main()
+    print(
+        f"Отчет для пользователя: {formatted_name} ({user_age} л.)\n"
+        f"Твой Индекс Массы Тела:{bmi}\n"
+        f"Рекомендуемая норма воды: {water_liters:.1f} л. в день\n\n"
+        f"Расчет окончен. Будьте здоровы!",
+    )
